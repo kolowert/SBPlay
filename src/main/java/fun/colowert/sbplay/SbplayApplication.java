fb6a8c9a84b68e708e75902d00ae8e0286f71a69
Taras_Kohut <tiarkey@yahoo.com>
2022-02-21 22:02:19 +0200

    initial commit

---
+++ src/main/java/fun/colowert/sbplay/SbplayApplication.java
@@ -0,0 +1,13 @@
+package fun.colowert.sbplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/colowert/sbplay/SbplayApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.colowert.sbplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
